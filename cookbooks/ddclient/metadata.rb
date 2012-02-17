cda4b689b249134baa5c41cde3b8abe5668c45e8
Alex Kiernan <alexk@alexandalex.com>
2012-02-17 21:31:22 +0000

    ddclient cookbook

---
+++ cookbooks/ddclient/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Alex Kiernan"
+maintainer_email "alexk@alexandalex.com"
+license          "Apache 2.0"
+description      "Installs/Configures ddclient"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
